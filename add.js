b30b8c0cfe55d17d6bd9b851a3afb9c320b83425
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-12-04 00:01:19 +0200

    add step 2

--- add.js
+++ add.js
@@ -3,7 +3,8 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 
 const getHtml = async (url) => {
-  const { data } = await axios.get(url);
+  const link = url.startsWith("https://") ? url : `https://${url}`;
+  const { data } = await axios.get(link);
 
   return cheerio.load(data);
 };
@@ -16,35 +17,35 @@ const searchEmail = async (url) => {
     const $ = await getHtml(url);
 
     $("a").each((i, elem) => {
-      const href = $(elem).attr("href");
+      const href = $(elem).attr("href").replace(/\?.+/g, "");
 
-      if (href === undefined || href.includes("tel") || href.includes("#")) {
+      if (href === undefined || href.startsWith("tel:") || href.includes("#")) {
         return;
       }
 
-      if (href.includes("@" && "mailto")) {
+      if (href.startsWith("mailto:")) {
         const indexEl = href.indexOf(":") + 1;
         arr.add(href.slice(indexEl));
         return;
       }
-      const link = href.replace(/\?.+/g, "");
 
-      if (link.indexOf("/") === 0) {
-        nabor.push(`${url}${link}`);
+      console.log(href);
+
+      if (href.startsWith("/")) {
+        nabor.push(`${url}${href}`);
       } else {
-        nabor.push(link);
+        nabor.push(href);
       }
     });
-
     return nabor;
   } catch (er) {
     console.log(er);
   }
 };
 
-// const myLink = "https://comparus.de";
+const myLink = "comparus.de";
 // const myLink = "https://www.visartech.com";
-const myLink = "https://madappgang.com";
+// const myLink = "madappgang.com";
 
 const parse = async (url) => {
   const elems = await searchEmail(url);
@@ -52,23 +53,14 @@ const parse = async (url) => {
   const uniq = new Set(elems);
   const link = Array.from(uniq);
 
-  let res = [];
-  for (let i = 0; i < link.length; i++) {
-    const es = await searchEmail(link[i]);
-    if (es !== undefined) {
-      res = [...res, ...es];
-    }
-  }
-  ///відкінуті силки з link2 якщо воні є в link
-  //строка з //
-  const res2 = new Set(res);
-  const link2 = Array.from(res2);
-  console.log(link);
-  console.log(link2);
+  async function search2link(dataArray) {
+    const multipleRequest = await Promise.all(
+      dataArray.map((item) => searchEmail(item))
+    );
 
-  // for (let i = 0; i < link2.length; i++) {
-  //   const es = await searchEmail(link2[i]);
-  // }
+    // console.log(multipleRequest.flat());
+  }
+  search2link(link);
 
   // console.log(arr);
 };
